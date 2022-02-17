7049fb0dbcebbdd8b23afb0bb326e3f6e5cdca69
jayjay <jayjaylabadan@gmail.com>
2022-02-17 21:09:29 +0800

    (feat) completed ColorPicker reusable component

--- components/form/ColorPicker.jsx
+++ components/form/ColorPicker.jsx
@@ -1,30 +1,42 @@
 import classNames from "classnames";
-import { useRef, useState, useCallback} from "react";
+import { useRef, useState, useCallback } from "react";
 import { HexColorPicker, HexColorInput } from "react-colorful";
 
 import useClickOutside from "../../hooks/useClickOutside";
 import styles from "./ColorPicker.module.scss";
 
-const ColorPicker = ({ label, color = "FFFFFF" }) => {
-  const [isOpen, toggle] = useState(false);
+const ColorPicker = ({ label, defaultColor = "FFFFFF" }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [color, setColor] = useState(defaultColor);
   const popover = useRef();
+  const [focused, setFocused] = useState(false); 
 
-  const close = useCallback(() => toggle(false), []);
+  const close = useCallback(() => setIsOpen(false), []);
   useClickOutside(popover, close);
+
+  console.log(color)
 
   return (
     <>
-      <div className="label">{label}</div>
-      <div className={classNames("d-flex", styles.picker)}>
-        <div
-          className={styles.swatch}
-          style={{ backgroundColor: color }}
-          onClick={() => toggle(true)}
+      <div className="label form-field">{label}</div>
+      <div className={classNames("d-flex", styles.picker, focused && styles.isFocused)}>
+        <div className={styles.swatchContainerstyles}>
+          <div
+            className={styles.swatch}
+            style={{ backgroundColor: color }}
+            onClick={() => setIsOpen(true)}
+          />
+        </div>
+        <HexColorInput
+          color={color}
+          className={styles.input}
+          onChange={setColor}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
         />
-        <HexColorInput color={color} className="Polaris-TextField__Input"/>
         {isOpen && (
-          <div className="popover" ref={popover}>
-            <HexColorPicker color={color} onChange={onChange} />
+          <div className={styles.popover} ref={popover}>
+            <HexColorPicker color={color} onChange={setColor} />
           </div>
         )}
       </div>
