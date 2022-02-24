af782e47ba3842dfa8d9ef31c1d4e6ea1279677c
jayjay <jayjaylabadan@gmail.com>
2022-02-24 19:16:45 +0800

    (fix) remove logo

--- pages/_app.js
+++ pages/_app.js
@@ -63,21 +63,7 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, host } = this.props;
     return (
-      <AppProvider
-        i18n={translations}
-        linkComponent={Link}
-        theme={{
-          logo: {
-            width: 124,
-            topBarSource:
-              "https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999",
-            contextualSaveBarSource:
-              "https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-gray.svg?6215648040070010999",
-            url: "http://jadedpixel.com",
-            accessibilityLabel: "Jaded Pixel",
-          },
-        }}
-      >
+      <AppProvider i18n={translations} linkComponent={Link}>
         <Provider
           config={{
             apiKey: API_KEY,
