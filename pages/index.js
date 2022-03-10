c7f810e7af1b4bc44e9bfc16d0d3764d0ae86360
dev-shakib <h@lskit.com>
2022-03-10 23:37:34 +0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 import { Heading, Page } from '@shopify/polaris';
 
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
+
+const app = new Koa();
+const router = new KoaRouter();
+
+// Json prettier middle ware
+app.use(json());
+
+// Simple middleware
+app.use(async (ctx) => (ctx.body = { msg: 'Hello World' }));
+
 const Index = () => (
   <Page>
     <Heading>Shopify app with Node and React 🎉</Heading>
