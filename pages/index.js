41c22c236701f4b2831ae4ff95c8ab20fecda2e6
dev-shakib <h@lskit.com>
2022-03-11 07:08:04 +0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,40 @@ const Index = () => (
   <Page>
     <Heading>Shopify app with Node and React 🎉</Heading>
 
-    <Card sectioned>
-      <EmptyState
-        heading="Manage your inventory transfers"
-        action={{ content: 'Add transfer' }}
-        secondaryAction={{
-          content: 'Learn more',
-          url: 'https://help.shopify.com',
-        }}
-        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-      >
-        <p>Track and receive your incoming inventory from suppliers.</p>
-      </EmptyState>
-    </Card>
+    function AccountConnectionExample() {
+  const [connected, setConnected] = useState(false);
+  const accountName = connected ? 'Jane Appleseed' : '';
+
+  const handleAction = useCallback(() => {
+    setConnected((connected) => !connected);
+  }, [connected]);
+
+  const buttonText = connected ? 'Disconnect' : 'Connect';
+  const details = connected ? 'Account connected' : 'No account connected';
+  const terms = connected ? null : (
+    <p>
+      By clicking <strong>Connect</strong>, you agree to accept Sample App’s{' '}
+      <Link url="Example App">terms and conditions</Link>. You’ll pay a
+      commission rate of 15% on sales made through Sample App.
+    </p>
+  );
+
+  return (
+    <AccountConnection
+      accountName={accountName}
+      connected={connected}
+      title="Example App"
+      action={{
+        content: buttonText,
+        onAction: handleAction,
+      }}
+      details={details}
+      termsOfService={terms}
+    />
+  );
+}
+    
+
   </Page>
 );
 
